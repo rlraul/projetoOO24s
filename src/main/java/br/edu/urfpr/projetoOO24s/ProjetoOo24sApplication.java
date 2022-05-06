e418a97e0ad08208760db0601ef8894382ea3623
rlraul <raul.cardoso@outlook.com>
2022-05-06 19:48:06 -0300

    Initial commit

---
+++ src/main/java/br/edu/urfpr/projetoOO24s/ProjetoOo24sApplication.java
@@ -0,0 +1,13 @@
+package br.edu.urfpr.projetoOO24s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoOo24sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoOo24sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/urfpr/projetoOO24s/ProjetoOo24sApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.urfpr.projetoOO24s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoOo24sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
